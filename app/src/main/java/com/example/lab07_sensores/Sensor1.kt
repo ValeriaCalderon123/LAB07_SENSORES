29c38b3f6874f896a84a301f18da18f9296a653d
ValeriaCalderon123 <valerianicoll.calderonr@gmail.com>
2022-06-27 22:05:05 -0500

    Cambios UI

---
+++ app/src/main/java/com/example/lab07_sensores/Sensor1.kt
@@ -0,0 +1,11 @@
+package com.example.lab07_sensores
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sensor1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sensor1)
+    }
+}
