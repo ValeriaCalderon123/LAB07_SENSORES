a9a591bf9c6d96d788f8d8d801786ce98291d19c
diegoflores20173372 <dflorescam@unsa.edu.pe>
2022-06-29 21:45:19 -0500

    Setting the button

--- app/src/main/java/com/example/lab07_sensores/MainActivity.kt
+++ app/src/main/java/com/example/lab07_sensores/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.lab07_sensores
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSensor1: Button = findViewById(R.id.btnSensor1)
+        val btnSensor2: Button = findViewById(R.id.btnSensor2)
+
+        btnSensor1.setOnClickListener {
+            val intent = Intent(this, Sensor1::class.java)
+            startActivity(intent)
+        }
+
+        btnSensor2.setOnClickListener {
+            val intent = Intent(this, Sensor2::class.java)
+            startActivity(intent)
+        }
+
     }
 }
